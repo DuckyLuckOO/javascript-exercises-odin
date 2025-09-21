ac21c3786a5b2d069fd463d38ff0e0a9a46ae59c
DuckyLuckOO <duckyluck@tutamail.com>
2025-09-21 19:14:55 +0200

    feat: Exercise 9 Finished
    - Learned the Number.isInteger() method

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) ){
+        return "ERROR";
+    }
+    let result = 0;
+    if (a > b) {
+        let c = b;
+        b = a;
+        a = c;
+    }
+    for (let i = a; i <=b; i++) {
+        result += i;
+    }
+    return result;
 
 };
 

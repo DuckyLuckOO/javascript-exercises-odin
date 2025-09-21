5fd054215817cba69abf08928865efb8ccdc22d5
DuckyLuckOO <duckyluck@tutamail.com>
2025-09-21 19:06:03 +0200

    feat: Exercise 8 Finished
    - Learned the "for arg of args" syntax in js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...others) {
+    let result = arr;
+    for (let char of others) {
+        result = result.filter(x  => x !== char);
+    }
+    return result;
 };
 
 // Do not edit below this line
